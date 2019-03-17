25609d5348dc63d64667722ef3944876f2cefddf
Vikash Kaul <Vikash@anviks-mac-pro.lan>
2019-03-17 19:53:27 -0400

    module2

--- src/app.js
+++ src/app.js
@@ -10,6 +10,18 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('index', {title: 'Index'}));
+const accountData = fs.readFileSync(
+  path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+);
+
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(
+  path.join(__dirname, 'json', 'users.json'), 'utf8'
+);
+
+const users = JSON.parse(userData);
+
+app.get('/', (req, res) => res.render('index', {title: 'Account Summary' , accounts}));
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
