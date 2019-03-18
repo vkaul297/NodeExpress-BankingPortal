a6a031f51729ea0e18eef4ea881d420386e94b95
Vikash Kaul <Vikash@anviks-mac-pro.lan>
2019-03-18 14:00:20 -0400

    updates

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.post('/transfer', (req, res) => {
   req.body.amount;
   accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) +
   parseInt(req.body.amount, 10);
-  const accountsJSON = JSON.stringify(accounts, null 4);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
   fs.writeFileSync(path.join (__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
   res.render('transfer', {message: 'Transfer Completed'});
 })
@@ -50,7 +50,7 @@ app.get('/payment', (req, res) => res.render('payment'), {account: accounts.cred
 app.post('/payment', (req, res) => {
   accounts.credit.balance -= req.body.amount;
   accounts.credit.available += parseInt(req.body.amount, 10);
-  const accountsJSON = JSON.stringify(accounts, null 4);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
   fs.writeFileSync(path.join (__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
   res.render('payment', {message: 'Payment Successful', account: accounts.credit});
 })
