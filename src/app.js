012596343a1a1b0280e7d4b8853b5c2d1c93343e
Vikash Kaul <Vikash@anviks-mac-pro.lan>
2019-03-18 14:05:39 -0400

    test

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ app.post('/transfer', (req, res) => {
   res.render('transfer', {message: 'Transfer Completed'});
 })
 
-app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit});
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
 app.post('/payment', (req, res) => {
   accounts.credit.balance -= req.body.amount;
   accounts.credit.available += parseInt(req.body.amount, 10);
