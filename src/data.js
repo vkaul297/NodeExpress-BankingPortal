b7b37f3eca84e0e592a414e11033574cc3cf9c33
Vikash Kaul <Vikash@anviks-mac-pro.lan>
2019-03-22 14:30:52 -0400

    typo

--- src/data.js
+++ src/data.js
@@ -12,7 +12,7 @@ const userData = fs.readFileSync(
 const users = JSON.parse(userData);
 
 const writeJSON = () => {
-  onst accountsJSON = JSON.stringify(accounts, null, 4);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
   fs.writeFileSync(path.join (__dirname, 'json/accounts.json'),
   accountsJSON,'utf8');
 };
